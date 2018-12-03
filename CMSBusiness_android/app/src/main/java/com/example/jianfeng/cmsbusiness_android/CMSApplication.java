bf1ae32044f57f9990060f781ee7039ed2391ee3
jianfeng <497609288@qq.com>
2018-12-03 23:06:38 +0800

    begin

---
+++ CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/CMSApplication.java
@@ -0,0 +1,14 @@
+package com.example.jianfeng.cmsbusiness_android;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CMSApplication extends Application {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_cmsapplication);
+//    }
+}
