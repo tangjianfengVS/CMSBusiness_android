f43e44012771a73f3f3e2b8c07525c5dd4f0c01b
jianfeng <497609288@qq.com>
2018-12-07 23:03:48 +0800

    Hander

---
+++ CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/hander/CMSClickHander.java
@@ -0,0 +1,11 @@
+package com.example.jianfeng.cmsbusiness_android.hander;
+
+/**
+ * Created by jianfeng on 18/12/7.
+ */
+public interface CMSClickHander {
+
+    void clickBtnHander();
+
+    void clickBtnHander(String name, String passwork);
+}
