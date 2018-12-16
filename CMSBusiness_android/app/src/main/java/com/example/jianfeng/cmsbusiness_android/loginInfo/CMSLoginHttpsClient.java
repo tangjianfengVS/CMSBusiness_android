26cc52406e446044444bb9671de5f48ae3091753
jianfeng <497609288@qq.com>
2018-12-16 14:52:05 +0800

    更新

--- CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/loginInfo/CMSLoginHttpsClient.java
+++ CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/loginInfo/CMSLoginHttpsClient.java
@@ -1,14 +1,10 @@
 package com.example.jianfeng.cmsbusiness_android.loginInfo;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 
 /**
  * Created by jianfeng on 18/12/14.
  */
-public class CMSLoginHttpsClient extends OkHttpClient {
-
-
+public class CMSLoginHttpsClient{
 
 
 
--- CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/loginInfo/CMSLoginNI.java
+++ CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/loginInfo/CMSLoginNI.java
@@ -20,7 +20,6 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
@@ -33,24 +32,7 @@ import okhttp3.Request;
  */
 public class CMSLoginNI {
 
-    //private Request.Builder builder;
-
     private Context context;
-
-//    OkHttpClient okHttpClient = new OkHttpClient.Builder()
-//            .sslSocketFactory(SSLHelper.getSSLCertifcation(context))//为OkHttp对象设置SocketFactory用于双向认证
-//            .hostnameVerifier(new UnSafeHostnameVerifier())
-//            .build();
-//
-//    Retrofit retrofit = new Retrofit.Builder()
-//            .baseUrl("https://10.2.8.56:8443")
-//            .addConverterFactory(GsonConverterFactory.create())//添加 json 转换器
-//            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())//添加 RxJava 适配器
-//            .client(okHttpClient)//添加OkHttp代理对象
-//            .build();
-
-
-
 
 
     /** 登录 */
@@ -69,34 +51,34 @@ public class CMSLoginNI {
         this.context = context;
         String p = password;
 
-        //try {
-            /** 加密类型设置 */
-            MessageDigest sha = MessageDigest.getInstance("SHA-256");
-            byte[] b = p.getBytes();
-            sha.update(b, 0, b.length);
-            p = new String(Hex.encodeHex(sha.digest()));
+        /** 加密类型设置 */
+        MessageDigest sha = MessageDigest.getInstance("SHA-256");
+        byte[] b = p.getBytes();
+        sha.update(b, 0, b.length);
+        p = new String(Hex.encodeHex(sha.digest()));
 
-            Map<String, String> params = new HashMap<String, String>();
-            params.put("userpk", username);
-            params.put("e", eid);
+        /** 验证参数 */
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("userpk", username);
+        params.put("e", eid);
 
-            if (context != null) {
-                params.put("deviceid", deviceid);
-                params.put("devicemodel", String.valueOf(android.os.Build.BRAND) + String.valueOf(android.os.Build.MODEL) + String.valueOf(android.os.Build.VERSION.RELEASE));
-                //params.put("deviceid","aab3d27c2c4d2ef0");
-                //params.put("devicemodel","samsungSM-G93507.0");
-                //params.put("deviceid","pc");
-            }
+        if (context != null) {
+            params.put("deviceid", deviceid);
+            params.put("devicemodel", String.valueOf(android.os.Build.BRAND) + String.valueOf(android.os.Build.MODEL) + String.valueOf(android.os.Build.VERSION.RELEASE));
+            //params.put("deviceid","aab3d27c2c4d2ef0");
+            //params.put("devicemodel","samsungSM-G93507.0");
+            //params.put("deviceid","pc");
+        }
+
 
         //URL  https://auth.cshuanyu.com/auth
-        //JsonRpcHttpClient client = new JsonRpcHttpClient(
-        //new URL("https://auth.cshuanyu.com/auth"));
+        //JsonRpcHttpClient client = new JsonRpcHttpClient( new URL("https://auth.cshuanyu.com/auth"));
 
         //初始化全局 okhttpclient
-        CMSLoginHttpsClient client = (CMSLoginHttpsClient) new CMSLoginHttpsClient.Builder()
-                                                               .connectTimeout(20, TimeUnit.SECONDS)//设置超时时间
-                                                               .readTimeout(10, TimeUnit.SECONDS)
-                                                               .build();
+//        CMSLoginHttpsClient client = (CMSLoginHttpsClient) new CMSLoginHttpsClient.Builder()
+//                                                               .connectTimeout(20, TimeUnit.SECONDS)//设置超时时间
+//                                                               .readTimeout(10, TimeUnit.SECONDS)
+//                                                               .build();
         //发送get请求
         Request request =  new Request.Builder().get().url("https://auth.cshuanyu.com/auth").build();
 
@@ -105,6 +87,7 @@ public class CMSLoginNI {
         sslContext.init(null, new TrustManager[]{truseAllManager}, new SecureRandom());
 
         //client.setSslContext(sslContext);
+
 
 //            client.setSslContext(sslContext);
 //            client.setConnectionTimeoutMillis(2000);
@@ -127,25 +110,25 @@ public class CMSLoginNI {
 
         //密码验证
 //            ret = client.invoke("setA", params, AuthResult.class);
-//            B = ret.getValue("B").toString();
-//            session.setServerPublicKey_B(new BigInteger(B.substring(2, B.length() - 1), 16));
-//
-//            params.clear();
-//            params.put("userpk", username);
-//            params.put("e", eid);
-//            String M1 = session.getEvidenceValue_M1().toString(16);
-//            params.put("M1v", M1);
-//            //数据加密保存
-//            Map<String, Object> map = new HashMap();
-////            map= client.invoke("setM1", params, LinkedHashMap.class);
+        B = ret.getValue("B").toString();
+        session.setServerPublicKey_B(new BigInteger(B.substring(2, B.length() - 1), 16));
+
+        params.clear();
+        params.put("userpk", username);
+        params.put("e", eid);
+        String M1 = session.getEvidenceValue_M1().toString(16);
+        params.put("M1v", M1);
+        //数据加密保存
+        Map<String, Object> map = new HashMap();
+//            map= client.invoke("setM1", params, LinkedHashMap.class);
 //            ret = client.invoke("setM1", params, AuthResult.class);
-//            map.put("cert", ret.getValue("cert"));
-//            map.put("security", ret.getValue("security"));
-//            map.put("expired", ret.getValue("expired"));
-//            map.put("error", ret.getErrMsg());
-//
-////            if (ret.getErrMsg() == null) {
-////                //M2 = ret.getValue("M2").toString();
+        map.put("cert", ret.getValue("cert"));
+        map.put("security", ret.getValue("security"));
+        map.put("expired", ret.getValue("expired"));
+        map.put("error", ret.getErrMsg());
+
+        if (ret.getErrMsg() == null) {
+//                //M2 = ret.getValue("M2").toString();
 ////                map.put("code", M1);
 ////                map.put("expired", ret.getValue("expired").toString());
 ////                map.put("cert", ret.getValue("cert"));
@@ -161,7 +144,7 @@ public class CMSLoginNI {
 ////                map.put("expired", ret.getValue("expired").toString());
 ////                map.put("cert", null);
 ////                map.put("error", "");
-////            }
+        }
 //
 ////            map.put("session", session);
 ////            map.put("userid", I);
