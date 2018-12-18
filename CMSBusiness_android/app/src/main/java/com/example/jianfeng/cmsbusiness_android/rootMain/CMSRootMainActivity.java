21a1059295c558fbeb53c0fdbc3599601d2ff466
jianfeng <497609288@qq.com>
2018-12-18 17:38:52 +0800

    gengxin

--- CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/rootMain/CMSRootMainActivity.java
+++ CMSBusiness_android/app/src/main/java/com/example/jianfeng/cmsbusiness_android/rootMain/CMSRootMainActivity.java
@@ -1,7 +1,6 @@
 package com.example.jianfeng.cmsbusiness_android.rootMain;
 
 import android.content.Context;
-import android.os.Looper;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
